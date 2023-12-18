3ee614fc5c5b3fee4cdd433206cb2adfe313dc6c
parkdongmin <qkrehdals321@naver.com>
2023-12-18 13:32:36 +0900

    Ui Update

--- app/src/main/java/com/toomanybytes/spectrum/fragment/MypageFragment.kt
+++ app/src/main/java/com/toomanybytes/spectrum/fragment/MypageFragment.kt
@@ -31,7 +31,7 @@ class MypageFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("asd","asdfffsssdddfff")
+        Log.d("asd","asdfffsssdddsssff")
     }
 
     override fun onCreateView(
