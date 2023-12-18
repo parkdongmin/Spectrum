15cb687d98632d3913551b43280b743ca2e328b2
parkdongmin <qkrehdals321@naver.com>
2023-12-18 13:27:34 +0900

    Spectrum UI Update

--- app/src/main/java/com/toomanybytes/spectrum/fragment/MypageFragment.kt
+++ app/src/main/java/com/toomanybytes/spectrum/fragment/MypageFragment.kt
@@ -31,7 +31,7 @@ class MypageFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("asd","asdfff")
+        Log.d("asd","asdfffsss")
     }
 
     override fun onCreateView(
