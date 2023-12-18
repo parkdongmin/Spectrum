5627d3a5009745217f4abadd7cb12f5c1a35d314
parkdongmin <qkrehdals321@naver.com>
2023-12-18 14:19:04 +0900

    Spectrum UI Update

--- app/src/main/java/com/toomanybytes/spectrum/fragment/MypageFragment.kt
+++ app/src/main/java/com/toomanybytes/spectrum/fragment/MypageFragment.kt
@@ -1,7 +1,6 @@
 package com.toomanybytes.spectrum.fragment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -31,16 +30,14 @@ class MypageFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("asd","asdfffsssdddsssff")
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_mypage, container, false)
-        binding.lifecycleOwner = this
-        binding.viewModel = viewModel
+        binding = FragmentMypageBinding.inflate(layoutInflater)
 
         binding.userProfile.clipToOutline = true
 
