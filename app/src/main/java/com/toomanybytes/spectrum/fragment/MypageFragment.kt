e3ebe0272078312fc2444022329e8b2fc7a659f3
parkdongmin <qkrehdals321@naver.com>
2023-12-18 13:23:46 +0900

    Spectrum UI Update

--- app/src/main/java/com/toomanybytes/spectrum/fragment/MypageFragment.kt
+++ app/src/main/java/com/toomanybytes/spectrum/fragment/MypageFragment.kt
@@ -1,6 +1,7 @@
 package com.toomanybytes.spectrum.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -30,7 +31,7 @@ class MypageFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d("asd","asd")
     }
 
     override fun onCreateView(
